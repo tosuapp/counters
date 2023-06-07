7a6b4b4f1a1a10320f7d9d4d0a1a9347215ff1f1
cyperdark <aburachenko2000@gmail.com>
2023-06-07 15:42:52 +0300

    new-counter: basic/unstable-rate

---
+++ counters/basic-unstable-rate/index.js
@@ -0,0 +1,49 @@
+// connecting to websocket
+const socket = new ReconnectingWebSocket("ws://127.0.0.1:24050/ws");
+
+
+// handle on open/close/error
+socket.onopen = () => console.log("Successfully Connected");
+
+socket.onclose = event => {
+    console.log("Socket Closed Connection: ", event);
+    socket.send("Client Closed!");
+};
+
+socket.onerror = error => console.log("Socket Error: ", error);
+
+
+
+// cache values here to prevent constant updating
+const cache = {
+    state: -1,
+    ur: 0,
+};
+
+
+
+// Smoouth numbers update
+const unstableRate = new CountUp('ur', 0, 0, 2, .5, { useEasing: true, useGrouping: true, separator: " ", decimal: "." });
+
+
+
+// receive message update from websocket
+socket.onmessage = event => {
+    try {
+        const data = JSON.parse(event.data);
+
+
+        if (cache.state !== data.menu.state) {
+            cache.state = data.menu.state;
+            document.getElementById('ur').style.opacity = tempState == 2 ? 1 : 0
+        };
+
+
+        if (cache.ur !== data.gameplay.hits.unstableRate) {
+            cache.ur = data.gameplay.hits.unstableRate;
+            unstableRate.update(data.gameplay.hits.unstableRate);
+        };
+    } catch (err) {
+        console.log(err);
+    };
+};
