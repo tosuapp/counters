5547f9b0ce07934f6fd1288d847909ef9180b72f
ck <21735205+cyperdark@users.noreply.github.com>
2024-02-13 07:58:51 +0300

    fixes

---
+++ counters/basic-unstable-rate/readme.md
@@ -0,0 +1,14 @@
+## basic-unstable-rate by cyperdark
+
+<a href="https://github.com/cyperdark" target="_blank"><img width="30" src="https://cdn-icons-png.flaticon.com/128/5968/5968866.png" /></a>
+
+|||
+| ------------- | ------------- |
+| Compatible with | gosu ,tosu |
+| Size | 300x300 |
+
+
+<video width="320" height="240" controls>
+  <source src="assets/videos/basic-unstable-rate.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
--- types.js
+++ types.js
@@ -1,242 +1,147 @@
-/**
- * @typedef {Object} Folders
- * @property {string} game - path to game folder
- * @property {string} skin - skin folder name
- * @property {string} songs - path to songs folder
- */
-
-/**
- * @typedef {Object} Settings
- * @property {boolean} showInterface - Indicates if interface is shown
- * @property {Folders} folders - folders configuration
- */
-
-/**
- * @typedef {Object} MainMenu
- * @property {number} bassDensity - bass density
- */
-
-/**
- * @typedef {Object} Time
- * @property {number} firstObj - first object time
- * @property {number} current - current time
- * @property {number} full - full time
- * @property {number} mp3 - mp3 time
- */
-
-/**
- * @typedef {Object} Metadata
- * @property {string} artist - artist name
- * @property {string} artistOriginal - original artist name
- * @property {string} title - song title
- * @property {string} titleOriginal - original song title
- * @property {string} mapper - mapper name
- * @property {string} difficulty - difficulty level
- */
-
-/**
- * @typedef {Object} BPM
- * @property {number} min - minimum BPM
- * @property {number} max - maximum BPM
- */
-
-/**
- * @typedef {Object} Stats
- * @property {number} AR - approach rate
- * @property {number} CS - circle size
- * @property {number} OD - overall difficulty
- * @property {number} HP - health points
- * @property {number} SR - star rating
- * @property {BPM} BPM - BPM information
- * @property {number} maxCombo - maximum combo
- * @property {number} fullSR - full star rating
- * @property {number} memoryAR - memory AR
- * @property {number} memoryCS - memory CS
- * @property {number} memoryOD - memory OD
- * @property {number} memoryHP - memory HP
- */
-
-/**
- * @typedef {Object} Path
- * @property {string} full - full path
- * @property {string} folder - folder path
- * @property {string} file - file path
- * @property {string} bg - background path
- * @property {string} audio - audio path
- */
-
-/**
- * @typedef {Object} BM
- * @property {Time} time - time information
- * @property {number} id - beatmap id
- * @property {number} set - set number
- * @property {string} md5 - MD5 hash
- * @property {number} rankedStatus - ranked status
- * @property {Metadata} metadata - metadata
- * @property {Stats} stats - stats
- * @property {Path} path - file paths
- */
-
-/**
- * @typedef {Object} Mods
- * @property {number} num - mods id
- * @property {string} str - mods string
- */
-
-/**
- * @typedef {Object} Strains
- * @property {string} name - strain name
- * @property {number[]} data - strain data
- */
-
-/**
- * @typedef {Object} StrainsAll
- * @property {Strains[]} series - strain series
- * @property {number[]} xaxis - x-axis data
- */
-
-/**
- * @typedef {Object} PP
- * @property {number} 95 - 95% PP
- * @property {number} 96 - 96% PP
- * @property {number} 97 - 97% PP
- * @property {number} 98 - 98% PP
- * @property {number} 99 - 99% PP
- * @property {number} 100 - 100% PP
- * @property {number[]} strains - strains
- * @property {StrainsAll} strainsAll - strains all
- */
-
-/**
- * @typedef {Object} Menu
- * @property {MainMenu} mainMenu - main menu configuration
- * @property {number} state - osu state
- * @property {number} gameMode - selected game mode
- * @property {number} isChatEnabled - Indicates whether chat is shown
- * @property {BM} bm - beatmap metadata
- * @property {Mods} mods - mods information
- * @property {PP} pp - performance points data
- */
-
-/**
- * @typedef {Object} Combo
- * @property {number} current - current combo
- * @property {number} max - maximum combo
- */
-
-/**
- * @typedef {Object} HP
- * @property {number} normal - normal HP
- * @property {number} smooth - smooth HP
- */
-
-/**
- * @typedef {Object} Grade
- * @property {string} current - current grade
- * @property {string} maxThisPlay - maximum grade achieved in the current play
- */
-
-/**
- * @typedef {Object} HitError
- * @property {number} 0 - amount of misses
- * @property {number} 50 - amount of 50's
- * @property {number} 100 - amount of 100's
- * @property {number} 300 - amount of 300's
- * @property {number} geki - amount of geki's
- * @property {number} katu - amount of katu's
- * @property {number} sliderBreaks - amount of slider breaks
- * @property {Grade} grade - grade information
- * @property {number} unstableRate - unstable rate
- * @property {number[]} hitErrorArray - hit error array
- */
-
-/**
- * @typedef {Object} KeyOverlay
- * @property {Object} k1 - k1 key overlay
- * @property {boolean} k1.isPressed - Indicates whether k1 key is pressed
- * @property {number} k1.count - count of k1 key presses
- * @property {Object} k2 - k2 key overlay
- * @property {boolean} k2.isPressed - Indicates whether k2 key is pressed
- * @property {number} k2.count - count of k2 key presses
- * @property {Object} m1 - m1 key overlay
- * @property {boolean} m1.isPressed - Indicates whether m1 key is pressed
- * @property {number} m1.count - count of m1 key presses
- * @property {Object} m2 - m2 key overlay
- * @property {boolean} m2.isPressed - Indicates whether m2 key is pressed
- * @property {number} m2.count - count of m2 key presses
- */
-
-/**
- * @typedef {Object} Slot
- * @property {string} name - player name
- * @property {number} score - score
- * @property {number} combo - combo
- * @property {number} maxCombo - maximum combo
- * @property {string} mods - mods
- * @property {number} h300 - amount of 300
- * @property {number} h100 - amount of 100
- * @property {number} h50 - amount of 50
- * @property {number} h0 - amount of misses
- * @property {number} team - team number
- * @property {number} position - position
- * @property {number} isPassing - Indicates whether player is passing
- */
-
-/**
- * @typedef {Object} Leaderboard
- * @property {boolean} hasLeaderboard - Indicates whether leaderboard is available
- * @property {boolean} isVisible - Indicates whether leaderboard is visible
- * @property {Object} ourplayer - information of our player
- * @property {string} ourplayer.name - player name
- * @property {number} ourplayer.score - score
- * @property {number} ourplayer.combo - combo
- * @property {number} ourplayer.maxCombo - maximum combo
- * @property {string} ourplayer.mods - mods
- * @property {number} ourplayer.h300 - amount of 300
- * @property {number} ourplayer.h100 - amount of 100
- * @property {number} ourplayer.h50 - amount of 50
- * @property {number} ourplayer.h0 - amount of misses
- * @property {number} ourplayer.team - team number
- * @property {number} ourplayer.position - position
- * @property {number} ourplayer.isPassing - Indicates whether player is passing
- * @property {Slot[]} slots - leaderboard slots
- */
-
-/**
- * @typedef {Object} Gameplay
- * @property {number} gameMode - game mode
- * @property {string} name - game name
- * @property {number} score - score
- * @property {number} accuracy - accuracy
- * @property {Combo} combo - combo information
- * @property {HP} hp - HP information
- * @property {HitError} hits - hit error information
- * @property {PP} pp - PP information
- * @property {string} pp.current - current amount of pp
- * @property {number} pp.fc - pp if fc
- * @property {number} pp.maxThisPlay - peak pp
- * @property {KeyOverlay} keyOverlay - key overlay configuration
- * @property {Leaderboard} leaderboard - leaderboard information
- */
-
-/**
- * @typedef {Object} ResultsScreen
- * @property {number} 0 - amount of misses
- * @property {number} 50 - amount of 50
- * @property {number} 100 - amount of 100
- * @property {number} 300 - amount of 300
- * @property {string} name - player name
- * @property {number} score - score
- * @property {number} maxCombo - maximum combo
- * @property {Mods} mods - mods configuration
- * @property {number} geki - geki hit count
- * @property {number} katu - katu hit count
- */
-
-/**
- * @typedef {Object} MyObject
- * @property {Settings} settings - settings
- * @property {Menu} menu - menu data
- * @property {Gameplay} gameplay - gameplay information
- * @property {ResultsScreen} resultsScreen - results screen information
+/** @typedef {object} json
+ * @property {object} settings
+ * @property {boolean} settings.showInterface
+ * @property {object} settings.folders
+ * @property {string} settings.folders.game
+ * @property {string} settings.folders.skin
+ * @property {string} settings.folders.songs
+ * @property {object} menu
+ * @property {object} menu.mainMenu
+ * @property {number} menu.mainMenu.bassDensity
+ * @property {number} menu.state
+ * @property {number} menu.gameMode
+ * @property {number} menu.isChatEnabled
+ * @property {object} menu.bm
+ * @property {object} menu.bm.time
+ * @property {number} menu.bm.time.firstObj
+ * @property {number} menu.bm.time.current
+ * @property {number} menu.bm.time.full
+ * @property {number} menu.bm.time.mp3
+ * @property {number} menu.bm.id
+ * @property {number} menu.bm.set
+ * @property {string} menu.bm.md5
+ * @property {number} menu.bm.rankedStatus
+ * @property {object} menu.bm.metadata
+ * @property {string} menu.bm.metadata.artist
+ * @property {string} menu.bm.metadata.artistOriginal
+ * @property {string} menu.bm.metadata.title
+ * @property {string} menu.bm.metadata.titleOriginal
+ * @property {string} menu.bm.metadata.mapper
+ * @property {string} menu.bm.metadata.difficulty
+ * @property {object} menu.bm.stats
+ * @property {number} menu.bm.stats.AR
+ * @property {number} menu.bm.stats.CS
+ * @property {number} menu.bm.stats.OD
+ * @property {number} menu.bm.stats.HP
+ * @property {number} menu.bm.stats.SR
+ * @property {object} menu.bm.stats.BPM
+ * @property {number} menu.bm.stats.BPM.common
+ * @property {number} menu.bm.stats.BPM.min
+ * @property {number} menu.bm.stats.BPM.max
+ * @property {number} menu.bm.stats.circles
+ * @property {number} menu.bm.stats.sliders
+ * @property {number} menu.bm.stats.spinners
+ * @property {number} menu.bm.stats.holds
+ * @property {number} menu.bm.stats.maxCombo
+ * @property {number} menu.bm.stats.fullSR
+ * @property {number} menu.bm.stats.memoryAR
+ * @property {number} menu.bm.stats.memoryCS
+ * @property {number} menu.bm.stats.memoryOD
+ * @property {number} menu.bm.stats.memoryHP
+ * @property {object} menu.bm.path
+ * @property {string} menu.bm.path.full
+ * @property {string} menu.bm.path.folder
+ * @property {string} menu.bm.path.file
+ * @property {string} menu.bm.path.bg
+ * @property {string} menu.bm.path.audio
+ * @property {object} menu.mods
+ * @property {number} menu.mods.num
+ * @property {string} menu.mods.str
+ * @property {object} menu.pp
+ * @property {number} menu.pp.95
+ * @property {number} menu.pp.96
+ * @property {number} menu.pp.97
+ * @property {number} menu.pp.98
+ * @property {number} menu.pp.99
+ * @property {number} menu.pp.100
+ * @property {number[]} menu.pp.strains
+ * @property {object} menu.pp.strainsAll
+ * @property {object[]} menu.pp.strainsAll.series
+ * @property {string} menu.pp.strainsAll.series.name
+ * @property {number[]} menu.pp.strainsAll.series.data
+ * @property {number[]} menu.pp.strainsAll.xaxis
+ * @property {object} gameplay
+ * @property {number} gameplay.gameMode
+ * @property {string} gameplay.name
+ * @property {number} gameplay.score
+ * @property {number} gameplay.accuracy
+ * @property {object} gameplay.combo
+ * @property {number} gameplay.combo.current
+ * @property {number} gameplay.combo.max
+ * @property {object} gameplay.hp
+ * @property {number} gameplay.hp.normal
+ * @property {number} gameplay.hp.smooth
+ * @property {object} gameplay.hits
+ * @property {number} gameplay.hits.0
+ * @property {number} gameplay.hits.50
+ * @property {number} gameplay.hits.100
+ * @property {number} gameplay.hits.300
+ * @property {number} gameplay.hits.geki
+ * @property {number} gameplay.hits.katu
+ * @property {number} gameplay.hits.sliderBreaks
+ * @property {object} gameplay.hits.grade
+ * @property {string} gameplay.hits.grade.current
+ * @property {string} gameplay.hits.grade.maxThisPlay
+ * @property {number} gameplay.hits.unstableRate
+ * @property {number[]} gameplay.hits.hitErrorArray
+ * @property {object} gameplay.pp
+ * @property {number} gameplay.pp.current
+ * @property {number} gameplay.pp.fc
+ * @property {number} gameplay.pp.maxThisPlay
+ * @property {object} gameplay.keyOverlay
+ * @property {object} gameplay.keyOverlay.k1
+ * @property {boolean} gameplay.keyOverlay.k1.isPressed
+ * @property {number} gameplay.keyOverlay.k1.count
+ * @property {object} gameplay.keyOverlay.k2
+ * @property {boolean} gameplay.keyOverlay.k2.isPressed
+ * @property {number} gameplay.keyOverlay.k2.count
+ * @property {object} gameplay.keyOverlay.m1
+ * @property {boolean} gameplay.keyOverlay.m1.isPressed
+ * @property {number} gameplay.keyOverlay.m1.count
+ * @property {object} gameplay.keyOverlay.m2
+ * @property {boolean} gameplay.keyOverlay.m2.isPressed
+ * @property {number} gameplay.keyOverlay.m2.count
+ * @property {object} gameplay.leaderboard
+ * @property {boolean} gameplay.leaderboard.hasLeaderboard
+ * @property {boolean} gameplay.leaderboard.isVisible
+ * @property {object} gameplay.leaderboard.ourplayer
+ * @property {string} gameplay.leaderboard.ourplayer.name
+ * @property {number} gameplay.leaderboard.ourplayer.score
+ * @property {number} gameplay.leaderboard.ourplayer.combo
+ * @property {number} gameplay.leaderboard.ourplayer.maxCombo
+ * @property {string} gameplay.leaderboard.ourplayer.mods
+ * @property {number} gameplay.leaderboard.ourplayer.h300
+ * @property {number} gameplay.leaderboard.ourplayer.h100
+ * @property {number} gameplay.leaderboard.ourplayer.h50
+ * @property {number} gameplay.leaderboard.ourplayer.h0
+ * @property {number} gameplay.leaderboard.ourplayer.team
+ * @property {number} gameplay.leaderboard.ourplayer.position
+ * @property {number} gameplay.leaderboard.ourplayer.isPassing
+ * @property {} gameplay.leaderboard.slots
+ * @property {boolean} gameplay._isReplayUiHidden
+ * @property {object} resultsScreen
+ * @property {number} resultsScreen.0
+ * @property {number} resultsScreen.50
+ * @property {number} resultsScreen.100
+ * @property {number} resultsScreen.300
+ * @property {string} resultsScreen.name
+ * @property {number} resultsScreen.score
+ * @property {number} resultsScreen.maxCombo
+ * @property {object} resultsScreen.mods
+ * @property {number} resultsScreen.mods.num
+ * @property {string} resultsScreen.mods.str
+ * @property {number} resultsScreen.geki
+ * @property {number} resultsScreen.katu
+ * @property {object} userProfile
+ * @property {boolean} userProfile.isConnected
  */
