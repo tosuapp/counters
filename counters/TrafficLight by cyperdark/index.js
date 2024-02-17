52031c6d50fe361a5a6b53abe34be5015a816a9c
ck <21735205+cyperdark@users.noreply.github.com>
2024-02-17 09:27:50 +0300

    TrafficLight by cyperdark

---
+++ counters/TrafficLight by cyperdark/index.js
@@ -0,0 +1,53 @@
+let socket = new ReconnectingWebSocket("ws://127.0.0.1:24050/ws");
+let mapid = document.getElementById('mapid');
+
+let bg = document.getElementById("bg");
+let pp = document.getElementById("pp");
+let hun = document.getElementById("green");
+let fifty = document.getElementById("purple");
+let miss = document.getElementById("red");
+
+
+socket.onopen = () => {
+	console.log("Successfully Connected");
+};
+
+socket.onclose = event => {
+	console.log("Socket Closed Connection: ", event);
+	socket.send("Client Closed!")
+};
+
+socket.onerror = error => {
+	console.log("Socket Error: ", error);
+};
+let tempState;
+let tempImg;
+socket.onmessage = event => {
+	let data = JSON.parse(event.data);
+	if (tempState !== data.menu.bm.path.full) {
+		tempState = data.menu.bm.path.full
+		bg.setAttribute('src', `http://127.0.0.1:24050/Songs/${data.menu.bm.path.full}?a=${Math.random(10000)}`)
+		mapid.innerHTML = data.menu.bm.id;
+	}
+	if (data.gameplay.pp.current != '') {
+		let ppData = data.gameplay.pp.current;
+		pp.innerHTML = Math.round(ppData) + "pp"
+	} else {
+		pp.innerHTML = 0 + "pp"
+	}
+	if (data.gameplay.hits[100] > 0) {
+		hun.innerHTML = data.gameplay.hits[100];
+	} else {
+		hun.innerHTML = 0
+	}
+	if (data.gameplay.hits[50] > 0) {
+		fifty.innerHTML = data.gameplay.hits[50];
+	} else {
+		fifty.innerHTML = 0
+	}
+	if (data.gameplay.hits[0] > 0) {
+		miss.innerHTML = data.gameplay.hits[0];
+	} else {
+		miss.innerHTML = 0
+	}
+}
--- counters/readme.md
+++ counters/readme.md
@@ -223,3 +223,17 @@
 <img src="/.github/images/simplistic by jassper0.jpg" /> <img src="/.github/gifs/simplistic by jassper0.gif" /> 
 
 
+## TrafficLight by cyperdark
+
+<a href="https://github.com/cyperdark" target="_blank"><img width="30" src="https://cdn-icons-png.flaticon.com/128/5968/5968866.png" /></a>  <a href="https://twitter.com/cpol_owo" target="_blank"><img width="30" src="https://cdn-icons-png.flaticon.com/128/733/733579.png" /></a>  <a href="https://discord.gg/rYHNggbhyY" target="_blank"><img width="30" src="https://cdn-icons-png.flaticon.com/128/5968/5968756.png" /></a>  
+
+|||
+| ------------- | ------------- |
+| For | ingame, obs-overlay
+| Compatible with | gosu, tosu |
+| Size |  245x380 |
+
+
+<img src="/.github/images/trafficlight by cyperdark.jpg" /> <img src="/.github/gifs/trafficlight by cyperdark.gif" /> 
+
+
