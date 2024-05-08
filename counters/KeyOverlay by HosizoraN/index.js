13ac6628956503f4456c4748fb3dd93696a65e90
Shino Katarina <xyntak123@gmail.com>
2024-05-08 22:08:07 +0700

    Revert "update feat settings to keyOverlay"
    This reverts commit 57230f12fdfdf33462185a232f9ec402b8be8c59.

--- counters/KeyOverlay by HosizoraN/index.js
+++ counters/KeyOverlay by HosizoraN/index.js
@@ -1,9 +1,5 @@
-import WebSocketManager from './socket.js';
-
-const socket = new WebSocketManager('127.0.0.1:24050');
-
-const cache = {};
-
+let socket = new ReconnectingWebSocket("ws://127.0.0.1:24050/websocket/v2/precise");
+let keys = document.getElementById('keys');
 let Key1Cont = document.getElementById('Key1Cont');
 let Key2Cont = document.getElementById('Key2Cont');
 let Mouse1Cont = document.getElementById('Mouse1Cont');
@@ -13,79 +9,57 @@ let k1 = new KeyOverlay('k1', 'k1Tiles', { speed: 0.2, keyTextId: "k1Text", keyN
     m1 = new KeyOverlay('m1', 'm1Tiles', { speed: 0.2, keyTextId: "m1Text", keyNameId: "key3"}),
     m2 = new KeyOverlay('m2', 'm2Tiles', { speed: 0.2, keyTextId: "m2Text", keyNameId: "key4"});
 
-socket.sendCommand('getSettings', encodeURI(window.COUNTER_PATH));
-socket.commands((data) => {
-    try {
-      const { command, message } = data;
+socket.onopen = () => {
+    console.log("Successfully Connected");
+};
 
-      if (command == 'getSettings') {
-        console.log(command, message);
-      };
+socket.onclose = event => {
+    console.log("Socket Closed Connection: ", event);
+    socket.send("Client Closed!")
+};
 
-      if (message['KeyPressColor'] != null) {
-        document.body.style.setProperty('--keyColor', message['KeyPressColor']);
-      };
-      if (message['KeyIsPressedColor'] != null) {
-        document.body.style.setProperty('--keyTap', message['KeyIsPressedColor']);
-      };
-      if (message['TextColor'] != null) {
-        document.body.style.setProperty('--textColor', message['TextColor']);
-      };
-      
-    } catch (error) {
-      console.log(error);
-    };
-  });
+socket.onerror = error => {
+    console.log("Socket Error: ", error);
+};
 
-socket.api_v2_precise((data) => {
+socket.onmessage = (event) => {
+    let data = JSON.parse(event.data);
 
-    if (cache['data.keys.k1.count'] != data.keys.k1.count) {
-        cache['data.keys.k1.count'] = data.keys.k1.count;
-        if (data.keys.k1.count > 0) {
-            Key1Cont.style.opacity = 1
-            Key1Cont.style.transform = 'translateY(0)';
-        }
-        else {
-            Key1Cont.style.opacity = 0
-            Key1Cont.style.transform = 'translateY(-10px)';
-        }
+    if (data.keys.k1.count > 0) {
+        Key1Cont.style.opacity = 1
+        Key1Cont.style.transform = 'translateY(0)';
     }
-    if (cache['data.keys.k2.count'] != data.keys.k2.count) {
-        cache['data.keys.k2.count'] = data.keys.k2.count;
-        if (data.keys.k2.count > 0) {
-            Key2Cont.style.opacity = 1
-            Key2Cont.style.transform = 'translateY(0)';
-        }
-        else {
-            Key2Cont.style.opacity = 0
-            Key2Cont.style.transform = 'translateY(-10px)';
-        }
+    else {
+        Key1Cont.style.opacity = 0
+        Key1Cont.style.transform = 'translateY(-10px)';
     }
-    if (cache['data.keys.m1.count'] != data.keys.m1.count) {
-        cache['data.keys.m1.count'] = data.keys.m1.count;
-        if (data.keys.m1.count > 0) {
-            Mouse1Cont.style.opacity = 1
-            Mouse1Cont.style.transform = 'translateY(0)';
-        }
-        else {
-            Mouse1Cont.style.opacity = 0
-            Mouse1Cont.style.transform = 'translateY(-10px)';
-        }
+    if (data.keys.k2.count > 0) {
+        Key2Cont.style.opacity = 1
+        Key2Cont.style.transform = 'translateY(0)';
     }
-    if (cache['data.keys.m2.count'] != data.keys.m2.count) {
-        cache['data.keys.m2.count'] = data.keys.m2.count;
-        if (data.keys.m2.count > 0) {
-            Mouse2Cont.style.opacity = 1
-            Mouse2Cont.style.transform = 'translateY(0)';
-        }
-        else {
-            Mouse2Cont.style.opacity = 0
-            Mouse2Cont.style.transform = 'translateY(-10px)';
-        }
+    else {
+        Key2Cont.style.opacity = 0
+        Key2Cont.style.transform = 'translateY(-10px)';
+    }
+    if (data.keys.m1.count > 0) {
+        Mouse1Cont.style.opacity = 1
+        Mouse1Cont.style.transform = 'translateY(0)';
+    }
+    else {
+        Mouse1Cont.style.opacity = 0
+        Mouse1Cont.style.transform = 'translateY(-10px)';
+    }
+    if (data.keys.m2.count > 0) {
+        Mouse2Cont.style.opacity = 1
+        Mouse2Cont.style.transform = 'translateY(0)';
+    }
+    else {
+        Mouse2Cont.style.opacity = 0
+        Mouse2Cont.style.transform = 'translateY(-10px)';
     }
 
     k1.update(data.keys.k1, "var(--keyColor)", "var(--keyTap)")
     k2.update(data.keys.k2, "var(--keyColor)", "var(--keyTap)")
     m1.update(data.keys.m1, "var(--keyColor)", "var(--keyTap)")
     m2.update(data.keys.m2, "var(--keyColor)", "var(--keyTap)")
-});
+}
