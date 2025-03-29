6610d773a1a3e79dc8c04fbc718576a5a78386f7
hizak <146826683+hizak-byte@users.noreply.github.com>
2025-03-29 15:16:20 +0900

    new: Fullcombo (#25)
    * Create metadata.txt
    
    * Add files via upload
    
    * Create socket.js
    
    * Create aiueo.txt
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete counters/Fullcombo by hizak/assets/aiueo.txt
    
    * Update metadata.txt
    
    * Update metadata.txt
    
    * formating + small fixes
    
    ---------
    
    Co-authored-by: ck <21735205+cyperdark@users.noreply.github.com>

---
+++ counters/Fullcombo by hizak/js/animationPlayer.js
@@ -0,0 +1,15 @@
+class AnimationPlayer {
+    constructor(img_element, apng, default_image) {
+        this.img_element = img_element
+        this.apng = apng;
+        this.default_image = default_image
+
+        this.reset_frame()
+    }
+    reset_frame() {
+        this.img_element.src = this.default_image
+    }
+    play() {
+        this.img_element.src = this.apng
+    }
+}
