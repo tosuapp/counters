56a08ae2591c37cff2b575f804b8c22d304ecc15
GabuTheDev <107962621+GabuTheDev@users.noreply.github.com>
2025-05-11 14:54:48 +0300

    feat: Apply filters to reduce data size 3/?

--- counters/MinimalLime by cyperdark/index.js
+++ counters/MinimalLime by cyperdark/index.js
@@ -8,6 +8,21 @@ let miss = document.getElementById("h0");
 
 socket.onopen = () => {
     console.log("Successfully Connected");
+    socket.send(`applyFilters:${JSON.stringify([
+        {
+            field: 'gameplay',
+            keys: [
+                {
+                    field: 'pp',
+                    keys: ['current']
+                },
+                {
+                    field: 'hits',
+                    keys: ['100', '50', '0']
+                }
+            ]
+        },
+    ])}`)
 };
 
 socket.onclose = event => {
