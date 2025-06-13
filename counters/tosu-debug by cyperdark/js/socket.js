ba8858d9551b8137ce049ac5438bd2df240e330c
ck <21735205+cyperdark@users.noreply.github.com>
2025-06-13 20:11:43 +0300

    update: tosu debug

--- counters/tosu-debug by cyperdark/js/socket.js
+++ counters/tosu-debug by cyperdark/js/socket.js
@@ -169,9 +169,9 @@ class WebSocketManager {
   };
 
   /**
-   * 
-   * @param {string} name 
-   * @param {string|Object} payload 
+   *
+   * @param {string} name
+   * @param {string|Object} payload
    */
   sendCommand(name, command, amountOfRetries = 1) {
     const that = this;
@@ -223,7 +223,7 @@ export default WebSocketManager;
 
 
 
-/** 
+/**
  * @typedef {string | { field: string; keys: Filters[] }} Filters
  */
 
@@ -569,14 +569,29 @@ export default WebSocketManager;
  * @property {boolean} settings.cursor.useSkinCursor
  * @property {boolean} settings.cursor.autoSize
  * @property {number} settings.cursor.size
+ * @property {number} settings.cursor.menuSize
  * @property {object} settings.mouse
+ * @property {boolean} settings.mouse.highPrecision
  * @property {boolean} settings.mouse.rawInput
  * @property {boolean} settings.mouse.disableButtons
  * @property {boolean} settings.mouse.disableWheel
  * @property {number} settings.mouse.sensitivity
+ * @property {object} settings.tablet
+ * @property {boolean} settings.tablet.enabled
+ * @property {number} settings.tablet.x
+ * @property {number} settings.tablet.y
+ * @property {number} settings.tablet.width
+ * @property {number} settings.tablet.height
+ * @property {number} settings.tablet.ratation
+ * @property {number} settings.tablet.pressureThreshold
  * @property {object} settings.mania
  * @property {boolean} settings.mania.speedBPMScale
  * @property {boolean} settings.mania.usePerBeatmapSpeedScale
+ * @property {boolean} settings.mania.usePerBeatmapSpeedScale
+ * @property {number} settings.mania.scrollSpeed
+ * @property {object} settings.mania.scrollDirection
+ * @property {number} settings.mania.scrollDirection.number
+ * @property {'up' | 'down'} settings.mania.scrollDirection.name
  * @property {object} settings.sort
  * @property {0 | 1 | 2 | 3 | 4 | 5 | 6 | 7} settings.sort.number
  * @property {'artist' | 'bpm' | 'creator' | 'date' | 'difficulty' | 'length' | 'rank' | 'title'} settings.sort.name
@@ -596,6 +611,7 @@ export default WebSocketManager;
  * @property {boolean} settings.audio.ignoreBeatmapSounds
  * @property {boolean} settings.audio.useSkinSamples
  * @property {object} settings.audio.volume
+ * @property {number} settings.audio.volume.masterInactive
  * @property {number} settings.audio.volume.master
  * @property {number} settings.audio.volume.music
  * @property {number} settings.audio.volume.effect
@@ -603,6 +619,7 @@ export default WebSocketManager;
  * @property {number} settings.audio.offset.universal
  * @property {object} settings.background
  * @property {number} settings.background.dim
+ * @property {number} settings.background.blur
  * @property {boolean} settings.background.video
  * @property {boolean} settings.background.storyboard
  * @property {object} settings.keybinds
