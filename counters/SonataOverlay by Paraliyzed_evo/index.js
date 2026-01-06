ebfca83a92f2576de1dd187fb6d5837cd26e9891
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2026-01-06 17:41:42 -0600

    Fix bug in userProfile being undefined

--- counters/SonataOverlay by Paraliyzed_evo/index.js
+++ counters/SonataOverlay by Paraliyzed_evo/index.js
@@ -488,7 +488,7 @@ socket.commands(async (data) => {
             }
           }
         
-        if (cache['profileColor'] !== userProfile.backgroundColour) {
+        if (userProfile && cache['profileColor'] !== userProfile.backgroundColour) {
             cache['profileColor'] = userProfile.backgroundColour;
             setprofileColor(cache['profileColor']);
         }
